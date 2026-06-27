16d6cc81b8ca5050eb9b6a7f0822936e2708ebb4
bonnati42 <bonfil2009@mail.ru>
2026-06-27 18:46:23 +0300

    Update savesystem.h

--- ConsoleRPG/savesystem.h
+++ ConsoleRPG/savesystem.h
@@ -1,13 +1,9 @@
 #ifndef SAVESYSTEM_H
 #define SAVESYSTEM_H
-
 #include <iostream>
 #include "character.h"
-
-bool LoadGame(Character& player, Character& enemy, const string& filename = "saves\\save.txt");
-
-bool SaveGame(const Character& player, const Character& enemy, const string& filename = "saves\\save.txt");
-
-bool SaveExists(const string& filename = "saves\\save.txt");
-
-#endif 
+#include "shop.h"
+bool LoadGame(Character& player, Character& enemy,const string& filename="saves\\save.txt");
+bool SaveGame(const Character& player,const Character& enemy,const Shop& shop,const string& filename="saves\\save.txt");
+bool SaveExists(const string& filename="saves\\save.txt");
+#endif
